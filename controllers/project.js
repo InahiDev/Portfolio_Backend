690f234f0c062222b3eb5f3ed958550ff3e708e1
Inahi <gabriel.delaigue@gmail.com>
2023-08-29 17:09:15 +0200

    Implémentation différenciées des créations de projets avec / sans overview / image, lorsqu'un seul fichier est présent, c'est dans la req.query imageType=overview ou image qu'est défini quelle entrée de project est à mettre à jour. Même utilisation pour getSamples => count= pour obtenir le nombre de smaples désiré

--- controllers/project.js
+++ controllers/project.js
@@ -21,49 +21,74 @@ exports.getSpecific = (req, res) => {
 }
 
 exports.getSamples = (req, res) => {
-  const samples = 4
+  const samples = req.query.count
   Project.count()
     .then(numberOfProjects => {
       if (numberOfProjects >= samples) {
         Project.findAll({ order: [['createdAt', 'DESC']], offset: (numberOfProjects - samples), limit: samples})
           .then(data => {
             res.status(200).json(data)})
-          .catch((error) => res.status(500).json({message:`Erreur lors de la sélection du nombre désiré de samples ${error}`}))
+          .catch((error) => res.status(500).json({message:`Error selecting the required sample count :${error}`}))
       } else {
         Project.findAll({ order: [['createdAt', 'DESC']] })
           .then(projects => res.status(200).json(projects))
           .catch((error) => res.status(500).json({message:`Had some trouble finding the few samples needed: ${error}`}))
       }
     })
-    .catch((error) => res.status(500).json({message: `Had trouble counting the projects inth DB: ${error}`}))
+    .catch((error) => res.status(500).json({message: `Had trouble counting the projects in the DB: ${error}`}))
 }
 
 exports.createProject = (req, res) => {
   if (req.files) {
-    const projectObject = JSON.parse(req.body.project)
-    const project = new Project({
-      description: projectObject.description,
-      teaching: projectObject.teaching,
-      overView: `${req.protocol}://${req.get('host')}/images/${req.files[0].filename}`,
-      image: `${req.protocol}://${req.get('host')}/images/${req.files[1].filename}`
-    })
-    console.log(projectObject.stacks)
-    for (const stack of projectObject.stacks) {
-      project[stack] = true
-    }
-    project.save()
-      .then((data) => res.status(201).json({ message: "Project created with overview and image linked!", data}))
-      .catch((error) => res.status(500).json({ message: `Creation of project failed: ${error}`}))
+    if (req.files.length > 1) {
+      const projectObject = JSON.parse(req.body.project)
+      const project = new Project({
+        description: projectObject.description,
+        teaching: projectObject.teaching,
+        overView: `${req.protocol}://${req.get('host')}/images/${req.files[0].filename}`,
+        image: `${req.protocol}://${req.get('host')}/images/${req.files[1].filename}`
+      })
+      if (projectObject.stacks.length) {
+        for (const stack of projectObject.stacks) {
+          project[stack] = true
+        }
+      }
+      project.save()
+        .then((data) => res.status(201).json({ message: "Project created with overview and image linked!", data}))
+        .catch((error) => res.status(500).json({ message: `Creation of project failed: ${error}`}))
+    } else if (req.files.length === 1) {
+        const projectObject = JSON.parse(req.body.project)
+        const project = new Project({
+          description: projectObject.description,
+          teaching: projectObject.teaching
+        })
+        if (projectObject.stacks.length) {
+          for (stack of projectObject.stacks) {
+            project[stack] = true
+          }
+        }
+        if (req.query.imageType === "image") {
+          project.image = `${req.protocol}://${req.get('host')}/images/${req.files[0].filename}`
+        } else if (req.query.imageType === "overview") {
+          project.overView = `${req.protocol}://${req.get('host')}/images/${req.files[0].filename}`
+        }
+        project.save()
+          .then((data) => res.status(201).json({ message: "Content created with only one image on two possible", data}))
+          .catch((error) => res.status(500).json({ message: `Creation of project failed: ${error}`}))
+    } 
   } else {
     const project = new Project({
       ...req.body
     })
-    let stacks = req.body.stacks
-    for (const stack of stacks) {
-      project[stack] = true
+    console.log(project)
+    if (req.body.stacks.length) {
+      let stacks = req.body.stacks
+        for (const stack of stacks) {
+          project[stack] = true
+        }
     }
     project.save()
-      .then((data) => res.status(201).json({ message: "Project created without overview or image linked!", data}))
+      .then((data) => res.status(201).json({ message: "Project created without overview nor image linked!", data}))
       .catch((error) => res.status(500).json({ message: `Creation of project failed: ${error}`}))
   }
 }
